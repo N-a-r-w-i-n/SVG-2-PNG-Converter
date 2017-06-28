b7ad044c372faae87e00286c42ede32b6e19be19
NARWIN <N-a-r-w-i-n@users.noreply.github.com>
2017-06-28 16:32:59 -0700

    Add project files.

---
+++ SVG 2 PNG Converter/Dialogs/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SVG_2_PNG_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(3, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(561, 368);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File";
+            this.columnHeader1.Width = 115;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Path";
+            this.columnHeader2.Width = 379;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addFilesToolStripMenuItem,
+            this.convertToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 70);
+            // 
+            // addFilesToolStripMenuItem
+            // 
+            this.addFilesToolStripMenuItem.Image = global::SVG_2_PNG_Converter.Properties.Resources.Gnome_List_Add_32;
+            this.addFilesToolStripMenuItem.Name = "addFilesToolStripMenuItem";
+            this.addFilesToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.addFilesToolStripMenuItem.Text = "Add Files";
+            this.addFilesToolStripMenuItem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // convertToolStripMenuItem
+            // 
+            this.convertToolStripMenuItem.Image = global::SVG_2_PNG_Converter.Properties.Resources.Gnome_View_Sort_Descending_32;
+            this.convertToolStripMenuItem.Name = "convertToolStripMenuItem";
+            this.convertToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.convertToolStripMenuItem.Text = "Convert";
+            this.convertToolStripMenuItem.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Image = global::SVG_2_PNG_Converter.Properties.Resources.Gnome_Emblem_Unreadable_32;
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::SVG_2_PNG_Converter.Properties.Resources.Gnome_Emblem_Unreadable_32;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(141, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::SVG_2_PNG_Converter.Properties.Resources.Gnome_View_Sort_Descending_32;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(432, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::SVG_2_PNG_Converter.Properties.Resources.Gnome_List_Add_32;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(3, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bmp",
+            "Gif",
+            "Icon",
+            "Jpeg",
+            "Png",
+            "Tiff"});
+            this.comboBox1.Location = new System.Drawing.Point(280, 379);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(146, 26);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 411);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SVG 2 PNG Converter";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ SVG 2 PNG Converter/Dialogs/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using Svg;
+
+/// <summary> 
+/// <Library="SVG"> This is the 3rd Party library compiled from : https://github.com/vvvv/SVG </Library>
+/// <Icon="Icon">https://www.iconfinder.com/icons/88561/filetype_png_icon#size=128</Icon>
+/// <Button_ContextIcons>https://www.iconfinder.com/iconsets/gnome-desktop-icons-png</Button_ContextIcons>
+/// </summary>
+
+namespace SVG_2_PNG_Converter
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// The Base Title of the form.
+        /// </summary>
+        private string Title
+        {
+            get
+            {
+                return "SVG 2 PNG Converter";
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 4;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog {Filter = "Scalable Vector Graphics File (*.svg)|*.svg", Multiselect = true})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    for (int i = 0; i <= ofd.FileNames.Length - 1; i++)
+                    {
+                        // here we convert the svg and put to the bitmap.
+                        using (Bitmap B = SvgDocument.Open(ofd.FileNames[i]).Draw())
+                        {
+                            // adding the current image to imagelist.
+                            AddImageToImageList(imageList1,Path.GetFileName(ofd.FileNames[i]), B);
+                            // getting the svg filename.
+                            string filename = Path.GetFileName(ofd.FileNames[i]);
+                            // adding the current image to listview.
+                            AddItemToListView(listView1, filename, new string[] { ofd.FileNames[i]}, imageList1);
+                        }
+                    }
+            }
+
+            Text = Title + string.Format(" : Items ({0}).", listView1.Items.Count);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var FBD = new FolderBrowserDialog())
+            {
+                if (FBD.ShowDialog() == DialogResult.OK)
+                    for (int i = 0; i <= listView1.Items.Count - 1; i++)
+                    {
+                        // here we convert the svg and put to the bitmap.
+                        using (Bitmap B = SvgDocument.Open(listView1.Items[i].SubItems[1].Text).Draw())
+                        {
+                            // We get the same svg file names and put to the png files.
+                            B.Save(FBD.SelectedPath + "/" + listView1.Items[i].Text.Replace(".svg", "." + comboBox1.SelectedItem.ToString()), ImgFormat(comboBox1.SelectedItem.ToString()));
+                        }
+                    }
+                // here we show the number of svg files converted.
+                MessageBox.Show(string.Format("{0} SVG Items Successfully Converted To PNG !", listView1.Items.Count) + Environment.NewLine + string.Format("Destination : {0}", FBD.SelectedPath));
+            }
+            
+        }
+
+        /// <summary>
+        /// Adds images to Imagelist
+        /// </summary>
+        /// <param name="imgList">the ImageList to add images</param>
+        /// <param name="imgName">name for the image in ImageList</param>
+        /// <param name="img">the image to add</param>
+        private void AddImageToImageList(ImageList imgList, string imgName, Image img)
+        {
+            imgList.Images.Add(imgName, img);
+        }
+
+        /// <summary>
+        /// Gets the ImageIndex in ImageList
+        /// </summary>
+        /// <param name="imgList">the ImageList which contains the the image to get index</param>
+        /// <param name="keyName">the image name inside the ImageList</param>
+        /// <returns></returns>
+        private int ImageListImageIndex(ImageList imgList, string keyName)
+        {
+            return imgList.Images.IndexOfKey(keyName);
+        }
+
+        /// <summary>
+        /// Adds and item to listview cotnrol.
+        /// </summary>
+        /// <param name="listView">The ListView to storing items.</param>
+        /// <param name="item">An Item to add.</param>
+        /// <param name="subitems">The Subitems of current item.</param>
+        /// <param name="imageList">The Imagelist for the listview.</param>
+        private void AddItemToListView(ListView listView, string item, string[] subitems, ImageList imageList)
+        {
+            var LVI = new ListViewItem();
+            LVI.Text = item;
+            LVI.SubItems.AddRange(subitems); //
+            LVI.ImageIndex = ImageListImageIndex(imageList, item);
+            listView.Items.Add(LVI);
+        }
+
+
+        /// <summary>
+        /// The Method to clear listview and returing the base title to the form.
+        /// </summary>
+        private void Clear()
+        {
+            // Clearing the items of the listview.
+            listView1.Items.Clear();
+            // returing the base title to the form.
+            Text = Title;
+        }
+
+
+        /// <summary>
+        /// The Method to return the ImageFormat based om limited ImageFormats in string.
+        /// </summary>
+        /// <param name="str">The ImageFormat in string.</param>
+        /// <returns>The Imageformat base on given string</returns>
+        private ImageFormat ImgFormat(string str)
+        {
+            switch (str)
+            {
+                case "Bmp":
+                    return ImageFormat.Bmp;
+                case "Gif":
+                    return ImageFormat.Gif;
+                case "Jpeg":
+                    return ImageFormat.Jpeg;
+                case "Png":
+                    return ImageFormat.Png;
+                case "Tiff":
+                    return ImageFormat.Tiff;
+                case "Icon":
+                    return ImageFormat.Icon;
+                default:
+                    return ImageFormat.Png;
+                    
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+    }
+}
